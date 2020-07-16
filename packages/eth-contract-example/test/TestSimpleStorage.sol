4da0e7dc1c2eb6cf71382421d5418df8e3b82440
xuyizhe <barrenbass@gmail.com>
2020-07-16 11:39:16 +0800

    Test TestSimpleStorage.sol

--- packages/eth-contract-example/test/TestSimpleStorage.sol
+++ packages/eth-contract-example/test/TestSimpleStorage.sol
@@ -4,4 +4,22 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/SimpleStorage.sol";
 
-contract TestSimpleStorage {}
+contract TestSimpleStorage {
+    function testInitialStorageData() public {
+        SimpleStorage simple = new SimpleStorage();
+
+        uint expected = 0;
+
+        Assert.equal(simple.get(), expected, "storedData should be 0 initially");
+    }
+
+    function testSet() public {
+        SimpleStorage simple = new SimpleStorage();
+
+        uint expected = 666;
+
+        simple.set(expected);
+
+        Assert.equal(simple.get(), expected, "storedData should be set to 666");
+    }
+}
